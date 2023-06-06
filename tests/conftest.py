f30a4d479ffc12f447f46fd0750cc7ba9d312a18
Ivo Zivkov <izivkov@gmail.com>
2023-06-05 22:50:49 -0400

    Added anaconda build

--- tests/conftest.py
+++ tests/conftest.py
@@ -8,25 +8,5 @@
 """
 
 import pytest
-
-class Fruit:
-    def __init__(self, name):
-        self.name = name
-
-    def __eq__(self, other):
-        return self.name == other.name
-
-
-@pytest.fixture
-def my_fruit():
-    return Fruit("apple")
-
-
-@pytest.fixture
-def fruit_basket(my_fruit):
-    return [Fruit("banana"), my_fruit]
-
-
-def test_my_fruit_in_basket(my_fruit, fruit_basket):
-    assert my_fruit in fruit_basket
+import gshocktimeserver
 
