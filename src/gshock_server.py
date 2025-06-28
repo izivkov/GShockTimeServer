a52a1e2346b9e3c6d7d7978ad3fd96feb86409f4
Just Me <izivkov@gmail.com>
2025-06-28 19:27:47 -0400

    WIP

--- src/gshock_server.py
+++ src/gshock_server.py
@@ -36,6 +36,43 @@ def prompt():
     logger.info("")
 
 
+async def show_display(api: GshockAPI):
+    from display.oled_simulator import MockOLEDDisplay
+
+    oled = MockOLEDDisplay()
+
+    try:
+        alarms = await api.get_alarms()
+        hour = alarms[0].get("hour")
+        minute = alarms[0].get("minute")
+        if isinstance(hour, int) and isinstance(minute, int):
+            alarm_str = f"{hour:02d}:{minute:02d}"
+        else:
+            alarm_str = "Invalid time"
+
+
+        reminders = await api.get_reminders()
+        reminder_title = reminders[0].get("title") or "No reminders"
+        condition = await api.get_watch_condition()
+        battery = condition.get("battery_level_percent")
+        temperature = condition.get("temperature")
+        name = await api.get_watch_name()
+        short_name = ' '.join(name.strip().split()[1:])
+
+        oled.show_status(
+            watch_name=short_name,
+            battery = battery,
+            temperature = temperature,
+            last_sync=datetime.now().strftime("%H:%M"),
+            alarm= alarm_str,
+            reminder=reminder_title,
+            auto_sync="On" if await api.get_time_adjustment() else "Off",
+        )
+        print("🖼 OLED preview saved as 'oled_preview.png'")
+
+    except Exception as e:
+        logger.error(f"Got error: {e}")
+
 async def run_time_server():
     prompt()
 
@@ -46,22 +83,19 @@ async def run_time_server():
             else:
                 address = conf.get("device.address")
 
-            device = await scanner.scan(address)
-            logger.debug("Found: {}".format(device))
-
-            connection = Connection(device)
+            logger.info(f"Before Connecting to watch at {address}...")
+            connection = Connection(address)
             await connection.connect()
+            logger.info(f"Connected to watch at {address}...")
 
             api = GshockAPI(connection)
-
             pressed_button = await api.get_pressed_button()
             if (
                 pressed_button != WatchButton.LOWER_RIGHT
                 and pressed_button != WatchButton.NO_BUTTON
+                and pressed_button != WatchButton.LOWER_LEFT
             ):
                 continue
-
-            await api.get_app_info()
 
             # Apply fine adjustment to the time
             fine_adjustment_secs = args.get().fine_adjustment_secs
@@ -71,8 +105,10 @@ async def run_time_server():
             
             logger.info(f"Time set at {datetime.now()} on {watch_info.name}")
 
-            # You can add mail notification here if you run your mail server.
-            # send_mail_notification(args.mailto)
+            # Only update the display of we have pressed LOWER-LEFT button,
+            # Otherwise the watch will dicoinnect before we get all the information for the display.
+            if pressed_button == WatchButton.LOWER_LEFT:
+                await show_display(api)
 
             if watch_info.alwaysConnected == False:
                 await connection.disconnect()
