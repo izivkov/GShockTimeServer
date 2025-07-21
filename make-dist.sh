f3bc9c8cd9a2cede30104d0059500332d391fdd2
Just Me <izivkov@gmail.com>
2025-07-20 23:03:01 -0400

    Updated the docs

--- make-dist.sh
+++ make-dist.sh
@@ -229,7 +229,7 @@ echo "Display type set to: $DISPLAY_TYPE"
 
 echo "[main]" > "$CONFIG_FILE"
 echo "display = $DISPLAY_TYPE" >> "$CONFIG_FILE"
-echo excluded_watches = ["OCW-S400-2AJF", "OCW-S400SG-2AJR", "OCW-T200SB-1AJF", "ECB-30", "ECB-20", "ECB-10", "ECB-50", "ECB-60", "ECB-70"]" >> "$CONFIG_FILE"
+echo excluded_watches = '["OCW-S400-2AJF", "OCW-S400SG-2AJR", "OCW-T200SB-1AJF", "ECB-30", "ECB-20", "ECB-10", "ECB-50", "ECB-60", "ECB-70"]' >> "$CONFIG_FILE"
 
 # end of config.ini update
 
